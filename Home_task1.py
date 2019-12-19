a8782d5a3279e03f8f8bc459ae18d69c18d70e2a
sandhiya97 <58969217+sandhiya97@users.noreply.github.com>
2019-12-19 11:37:22 +0530

    Add files via upload

---
+++ Home_task1.py
@@ -0,0 +1,7 @@
+#print the List of Natural Numbers from 1 to n
+number = int(input("Please Enter any Number: "))
+i = 1
+print("The List of Natural Numbers from 1 to n are") 
+while ( i <= number):
+    print (i, end=' ')
+    i = i + 1
